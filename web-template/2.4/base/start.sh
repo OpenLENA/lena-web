8c8db50448673017fd1f2910c6a64dc7fc5bdf45
서세진 책임/SWA Innovation팀 <sjsj@lgcns.com>
2023-05-19 15:53:46 +0900

    Feat: Apache engine and template packaging #6 start, stop script modified

--- web-template/2.4/base/start.sh
+++ web-template/2.4/base/start.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 
 . ${SCRIPTPATH}/env.sh
 
-${LAT_HOME}/management/*RELEASE/bin/latctl.sh start apache ${INSTANCE_ID}
+${LAT_HOME}/management/latctl/bin/latctl.sh start apache ${INSTANCE_ID}
 
--- web-template/2.4/base/stop.sh
+++ web-template/2.4/base/stop.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 
 . ${SCRIPTPATH}/env.sh
 
-${LAT_HOME}/management/*RELEASE/bin/latctl.sh stop apache ${INSTANCE_ID}
+${LAT_HOME}/management/latctl/bin/latctl.sh stop apache ${INSTANCE_ID}
 
