63f7a5e8207bfd533196d24a96afe507baaea002
youngK <tykim0123>
2023-12-18 09:54:33 +0900

    fix: change template env.sh comment : TEMPLATE_VERSION add

--- web-template/2.4.53/base/env.sh
+++ web-template/2.4.53/base/env.sh
@@ -19,6 +19,7 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 export LAT_HOME=/apps/lat/1.0.0
 export ENGN_VERSION=2.4.53
 export ENGN_HOME=${LAT_HOME}/engines/runtime/apache/${ENGN_VERSION}
+export TEMPLATE_VERSION=2.4.53
 export INSTANCE_ID=lat-apache
 export SERVICE_PORT=8580
 export RUN_USER=`whoami`
