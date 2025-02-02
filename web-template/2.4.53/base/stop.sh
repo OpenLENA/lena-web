b838d76059299a4636751cf39f8348481d9de7c2
moonct <tokybob@gmail.com>
2025-02-02 17:25:00 +0900

    feat: Update stop.sh

--- web-template/2.4.53/base/stop.sh
+++ web-template/2.4.53/base/stop.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 
 . ${SCRIPTPATH}/env.sh
 
-${LAT_HOME}/management/latctl/bin/latctl.sh stop apache ${INSTANCE_ID}
+${LAT_HOME}/latctl.sh stop apache ${INSTANCE_ID}
 
