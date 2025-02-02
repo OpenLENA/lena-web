5e2ae660b07290ef3b102a39d63fcb611d10b314
moonct <tokybob@gmail.com>
2025-02-02 17:17:22 +0900

    Update start.sh

--- web-template/2.4.53/base/start.sh
+++ web-template/2.4.53/base/start.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 
 . ${SCRIPTPATH}/env.sh
 
-${LAT_HOME}/management/latctl/bin/latctl.sh start apache ${INSTANCE_ID}
+${LAT_HOME}/latctl.sh start apache ${INSTANCE_ID}
 
